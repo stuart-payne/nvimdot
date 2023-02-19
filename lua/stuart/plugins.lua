ef235e2d0817e487cc20409392ffa9092964771f
Stuart Payne <paynestu@gmail.com>
2023-02-19 08:53:02 +0000

    use native formatting + rm neoformat

--- lua/stuart/plugins.lua
+++ lua/stuart/plugins.lua
@@ -8,7 +8,6 @@ return require('packer').startup(function(use)
 	use 'nvim-telescope/telescope.nvim'
 	use 'hrsh7th/cmp-nvim-lsp'
 	use 'hrsh7th/nvim-cmp'
-	use 'sbdchd/neoformat'
 	use {
 	  'nvim-lualine/lualine.nvim',
 	  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
