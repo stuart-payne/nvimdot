0a8f9e0f5b5966de13243a85e2b8dbf57c87d43b
Stuart Payne <paynestu@gmail.com>
2024-03-05 07:10:30 +0000

    plugin fix

--- lua/stuart/plugins.lua
+++ lua/stuart/plugins.lua
@@ -30,4 +30,18 @@ require("lazy").setup({
 		"stevearc/conform.nvim",
 		opts = {},
 	},
+	"L3MON4D3/LuaSnip",
+	"NvChad/nvim-colorizer.lua",
+	"numToStr/Comment.nvim",
+	"lewis6991/gitsigns.nvim",
+	{
+	  "nvim-lualine/lualine.nvim",
+	  dependencies = { "kyazdani42/nvim-web-devicons"}
+	},
+	"catppuccin/nvim",
+	{
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { {"nvim-lua/plenary.nvim"} }
+}
 })
